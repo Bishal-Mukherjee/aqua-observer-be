a1a4cce3ecd0759b33593a0b463390fe41c39f1c
Bishal-Mukherjee <bishalmukherjee448@gmail.com>
2025-07-16 00:55:55 +0530

    feat: Convert snake_case to camelCase for user details and add question routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import authRoutes from "@/routes/auth";
 import healthRoutes from "@/routes/health";
 import userRoutes from "@/routes/user";
 import sightingRoutes from "@/routes/sighting";
+import questionRoutes from "@/routes/questions";
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use("/auth", authRoutes);
 router.use("/health", authenticate, healthRoutes);
 router.use("/user", authenticate, userRoutes);
 router.use("/sighting", authenticate, sightingRoutes);
+router.use("/question", authenticate, questionRoutes);
 
 export { router };
