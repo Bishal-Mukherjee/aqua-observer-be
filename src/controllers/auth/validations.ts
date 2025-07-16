d3d807964e6eea7af8c6adc5101558dff4772618
Bishal-Mukherjee <bishalmukherjee448@gmail.com>
2025-07-17 00:56:09 +0530

    feat: Implement forgot password, OTP validation and reset password

--- src/controllers/auth/validations.ts
+++ src/controllers/auth/validations.ts
@@ -33,3 +33,34 @@ export const logoutSchema = Joi.object({
     "any.required": "Refresh token is required",
   }),
 });
+
+export const forgotPasswordSchema = Joi.object({
+  phoneNumber: Joi.string().required().messages({
+    "any.required": "Phone number is required",
+  }),
+});
+
+export const validateOtpSchema = Joi.object({
+  id: Joi.string().required().messages({
+    "any.required": "ID is required",
+  }),
+  code: Joi.string()
+    .pattern(/^[0-9]+$/)
+    .required()
+    .messages({
+      "any.required": "Code is required",
+    })
+    .min(6)
+    .message("Invalid code")
+    .max(6)
+    .message("Invalid code"),
+});
+
+export const resetPasswordSchema = Joi.object({
+  phoneNumber: Joi.string().required().messages({
+    "any.required": "Phone number is required",
+  }),
+  password: Joi.string().required().messages({
+    "any.required": "Password is required",
+  }),
+});
