de5e37f6dd0126be70a5e629bac87355654fa6cc
Bishal-Muherjee <bishalmukherjee448gmail.com>
2025-09-06 19:04:53 +0530

    feat: add SSL CA support for PostgreSQL connection string

--- src/config/config.ts
+++ src/config/config.ts
@@ -23,6 +23,9 @@ interface Config {
     serviceSid: string;
   };
   jwtSecret: string;
+  ssl: {
+    key: string;
+  };
 }
 
 const dbConfig = () => {
@@ -79,6 +82,16 @@ const twilioConfig = () => {
   };
 };
 
+const sslConfig = () => {
+  if (!process.env.SSL_KEY) {
+    throw new Error("Missing SSL configuration");
+  }
+
+  return {
+    key: process.env.SSL_KEY,
+  };
+};
+
 export const config: Config = {
   port: Number(process.env.SERVER_PORT) || 8080,
   nodeEnv: process.env.NODE_ENV || "development",
@@ -86,4 +99,5 @@ export const config: Config = {
   db: dbConfig(),
   redis: redisConfig(),
   twilio: twilioConfig(),
+  ssl: sslConfig(),
 };
