0d450a601051f9d4a2f63a211800c8d470560f8e
Bishal-Muherjee <bishalmukherjee448gmail.com>
2025-10-12 15:04:22 +0530

    feat: remove authHeader access using query in middleware

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -23,16 +23,14 @@ export const authenticate = async (
   res: Response,
   next: NextFunction,
 ) => {
-  const { accessToken = "" } = req.query;
-
   const authHeader = req.headers.authorization;
 
-  if ((!authHeader || !authHeader.startsWith("Bearer ")) && !accessToken) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     res.status(401).json({ message: "Unauthorized: Missing or invalid token" });
     return;
   }
 
-  const token = (authHeader?.split(" ")[1] || accessToken || "") as string;
+  const token = (authHeader?.split(" ")[1] || "") as string;
 
   try {
     const decoded = jwt.verify(token, config.jwtSecret) as jwt.JwtPayload;
