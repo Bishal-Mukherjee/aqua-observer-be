4c5d5c25b6b26855cc65bbe73f319c5dbc8a3ca4
Bishal Mukherjee <bishalmukherjee448@gmail.com>
2026-08-24 17:35:54 +0530

    chore: update rate limiter configuration to increase request limit and customize error response

--- src/utils/rate-limit.ts
+++ src/utils/rate-limit.ts
@@ -2,7 +2,12 @@ import { rateLimit } from "express-rate-limit";
 
 export const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 100,
+  limit: 150,
   standardHeaders: "draft-8",
   legacyHeaders: false,
+  handler: (req, res) => {
+    return res.status(429).json({
+      message: "Too many requests, please try again later.",
+    });
+  },
 });
